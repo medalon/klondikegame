d89fe5be1482dfc15f2650893257faf23f487140
Meder <medalon07@gmail.com>
2022-07-08 17:58:08 +0600

    add sprites

--- lib/klondike_game.dart
+++ lib/klondike_game.dart
@@ -1,9 +1,46 @@
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 
+import 'components/foundation.dart';
+import 'components/pile.dart';
+import 'components/stock.dart';
+import 'components/waste.dart';
+
 class KlondikeGame extends FlameGame {
+  static const double cardWidth = 1000.0;
+  static const double cardHeight = 1400.0;
+  static const double cardGap = 175.0;
+  static const double cardRadius = 100.0;
+  static final Vector2 cardSize = Vector2(cardWidth, cardHeight);
+
   @override
   Future<void>? onLoad() async {
     await Flame.images.load('klondike-sprites.png');
+
+    final stock = Stock()
+      ..size = cardSize
+      ..position = Vector2(cardGap, cardGap);
+
+    final waste = Waste()
+      ..size = cardSize
+      ..position = Vector2(cardWidth + 2 * cardGap, cardGap);
+
+    final foundations = List.generate(
+      4,
+      (i) => Foundation()
+        ..size = cardSize
+        ..position =
+            Vector2((i + 3) * (cardWidth + cardGap) + cardGap, cardGap),
+    );
+
+    final piles = List.generate(
+      7,
+      (i) => Pile()
+        ..size = cardSize
+        ..position = Vector2(
+          cardGap + i * (cardWidth + cardGap),
+          cardHeight + 2 * cardGap,
+        ),
+    );
   }
 }
